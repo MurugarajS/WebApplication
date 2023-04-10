861c4dd0712e7d5ce7a1bef7f1c57276b3f10306
Murugaraj <118675309+MurugarajS@users.noreply.github.com>
2023-04-10 10:10:46 +0530

    Add files via upload
    Show databases in GRID VIEW

---
+++ WebForm1.aspx
@@ -0,0 +1,222 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication3.Registration" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+
+    
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 32%;
+        }
+        .auto-style2 {
+            width: 199px;
+        }
+        .auto-style3 {
+            width: 578px;
+        }
+        .auto-style5 {
+            width: 119px;
+        }
+        .auto-style6 {
+            width: 119px;
+            height: 26px;
+        }
+        .auto-style7 {
+            height: 26px;
+        }
+        .auto-style10 {
+            width: 247px;
+        }
+        .auto-style11 {
+            width: 108px;
+        }
+        .auto-style14 {
+            width: 233px;
+        }
+        .auto-style15 {
+            width: 113px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <table class="auto-style1">
+
+                <caption>
+                    INSERTION</caption>
+                <tr>
+                    <td class="auto-style2">FIRSTNAME</td>
+                    <td class="auto-style3">
+                        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">LASTNAME</td>
+                    <td class="auto-style3">
+                        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">AGE</td>
+                    <td class="auto-style3">
+                        <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">
+                <asp:Button ID="Button1" runat="server" Text="Insert" Width="75px" Onclick="Submit_click" ForeColor="Green" />
+            </td>
+            <td class="auto-style3">&nbsp;</td>
+                </tr>
+            </table>
+        </div>
+        <table class="auto-style1">
+            <caption>
+                DELETION</caption>
+            <tr>
+                <td class="auto-style6">Id</td>
+                <td class="auto-style7">
+                        <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>
+                    </td>
+            </tr>
+            <tr>
+                <td class="auto-style5">
+                    <asp:Button ID="Button2" runat="server" Text="Delete" Width="75px" OnClick="Button2_Click" ForeColor="Red" />
+                </td>
+                <td>&nbsp;</td>
+            </tr>
+        </table>
+        <table class="auto-style1">
+            <caption>
+                UPDATION</caption>
+            <tr>
+                <td class="auto-style11">ID</td>
+                <td class="auto-style10">
+                        <asp:TextBox ID="TextBox8" runat="server"></asp:TextBox>
+                    </td>
+            </tr>
+            <tr>
+                <td class="auto-style11">FIRSTNAME</td>
+                <td class="auto-style10">
+                        <asp:TextBox ID="TextBox5" runat="server"></asp:TextBox>
+                    </td>
+            </tr>
+            <tr>
+                <td class="auto-style11">LASTNAME</td>
+                <td class="auto-style10">
+                        <asp:TextBox ID="TextBox6" runat="server"></asp:TextBox>
+                    </td>
+            </tr>
+            <tr>
+                <td class="auto-style11">AGE</td>
+                <td class="auto-style10">
+                        <asp:TextBox ID="TextBox7" runat="server"></asp:TextBox>
+                    </td>
+            </tr>
+            <tr>
+                <td class="auto-style11">
+                    <asp:Button ID="Button3" runat="server" Text="UPDATE" OnClick="Button3_Click" ForeColor="Orange" Width="107px" />
+                </td>
+                <td class="auto-style10">
+                    <asp:Button ID="Button4" runat="server" Text="CLEAR" OnClick="Button4_Click" Width="96px" />
+                </td>
+            </tr>
+        </table>
+        <table class="auto-style1">
+            <caption>
+                DETAILS</caption>
+            <tr>
+                <td class="auto-style15">Id</td>
+                <td class="auto-style14">
+                        <asp:TextBox ID="TextBox10" runat="server"></asp:TextBox>
+                    </td>
+            </tr>
+            <tr>
+                <td class="auto-style15">
+                    <asp:Button ID="Button5" runat="server" Text="GET" OnClick="Button5_Click" Width="95px" />
+                </td>
+                <td class="auto-style14"></td>
+                    &nbsp;</tr>
+        </table>
+       
+      <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" OnRowCommand="GridView1_RowCommand">
+    <Columns>
+        <asp:TemplateField HeaderText="FirstName" ItemStyle-Width="150">
+            <ItemTemplate>
+                <asp:TextBox ID="txtName" runat="server" Text='<%# Eval("FirstName") %>' />
+            </ItemTemplate>
+
+<ItemStyle Width="150px"></ItemStyle>
+        </asp:TemplateField>
+         
+
+         <asp:TemplateField HeaderText="LastName" ItemStyle-Width="150">
+            <ItemTemplate>
+                <asp:TextBox ID="txtlName" runat="server" Text='<%# Eval("LastName") %>' />
+            </ItemTemplate>
+
+<ItemStyle Width="150px"></ItemStyle>
+        </asp:TemplateField>
+
+
+
+         <asp:TemplateField HeaderText="Age" ItemStyle-Width="150">
+            <ItemTemplate>
+                <asp:TextBox ID="Age" runat="server" Text='<%# Eval("Age") %>' />
+            </ItemTemplate>
+
+<ItemStyle Width="150px"></ItemStyle>
+        </asp:TemplateField>
+
+         <asp:TemplateField HeaderText="Id" ItemStyle-Width="150">
+            <ItemTemplate>
+                <asp:TextBox ID="Id" runat="server" Text='<%# Eval("Auto_id") %>' />
+            </ItemTemplate>
+
+<ItemStyle Width="150px"></ItemStyle>
+        </asp:TemplateField>
+
+
+
+
+       
+        <asp:TemplateField>
+            <ItemTemplate>
+                <asp:Button Text="Update" runat="server" CommandName="Select" CommandArgument="<%# Container.DataItemIndex %>" />
+            </ItemTemplate>
+        </asp:TemplateField>
+
+         
+        <asp:TemplateField>
+            <ItemTemplate>
+                <asp:Button Text="Delete" runat="server" CommandName="Select1" CommandArgument="<%# Container.DataItemIndex %>" />
+            </ItemTemplate>
+        </asp:TemplateField>
+    </Columns>
+</asp:GridView>
+
+       
+
+    
+        
+      
+              
+   
+     
+    
+        
+      
+              
+   
+     
+</form>  
+          
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,319 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace WebApplication3
+{
+    public partial class Registration : System.Web.UI.Page
+    {
+       /* SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["WebForm1ConnectionString"].ConnectionString);
+
+        SqlCommand cmd = null;
+*/
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+
+            if (!this.IsPostBack)
+            {
+
+
+                gvbind();
+
+
+
+            }
+
+        }
+        protected void Submit_click(object sender, EventArgs e)
+        {
+
+
+            string cs = "Data Source=DESKTOP-NB93CBA\\MSSQLSERVER03;Initial Catalog=Registration;Integrated Security=True";
+            SqlConnection con = new SqlConnection(cs);
+
+            string query = "insert into dbo.data1(FirstName,LastName,Age)values('" + TextBox1.Text + "','" + TextBox2.Text + "','" + TextBox3.Text + "')";
+
+
+
+            SqlCommand cmd = new SqlCommand(query, con);
+
+            //cmd.Connection = con;
+
+            con.Open();
+
+            cmd.ExecuteNonQuery();
+
+
+
+            con.Close();
+
+            gvbind();
+            ClientScript.RegisterClientScriptBlock(this.GetType(), "K", "alert('Record Inserted! Thak you for your valuable time ..')", true);
+
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+
+            string cs = "Data Source=DESKTOP-NB93CBA\\MSSQLSERVER03;Initial Catalog=Registration;Integrated Security=True";
+            SqlConnection con = new SqlConnection(cs);
+
+            string query = "delete from dbo.data1 where Auto_id='" + TextBox4.Text + "'";
+
+            SqlCommand cmd = new SqlCommand(query, con);
+
+
+            //cmd.Connection = con;
+
+            con.Open();
+
+            try
+            {
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception Ex)
+            {
+                Console.Write("the user name not found");
+            }
+            con.Close();
+
+            gvbind();
+            ClientScript.RegisterClientScriptBlock(this.GetType(), "K", "alert('Record Deleted! Thak you for your valuable time ..')", true);
+
+
+
+        }
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+
+
+
+            string cs = "Data Source=DESKTOP-NB93CBA\\MSSQLSERVER03;Initial Catalog=Registration;Integrated Security=True";
+            SqlConnection con = new SqlConnection(cs);
+
+            string query = "update dbo.data1 set FirstName='" + TextBox5.Text + "',LastName='" + TextBox6.Text + "',Age='" + TextBox7.Text + "' where Auto_id='" + TextBox8.Text + "'";
+
+            SqlCommand cmd = new SqlCommand(query, con);
+
+
+            //cmd.Connection = con;
+
+            con.Open();
+
+            cmd.ExecuteNonQuery();
+
+            con.Close();
+
+            gvbind();
+
+            ClientScript.RegisterClientScriptBlock(this.GetType(), "K", "alert('Record Updated! Thak you for your valuable time ..')", true);
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+
+
+
+
+            TextBox1.Text = "";
+            TextBox2.Text = "";
+            TextBox3.Text = "";
+            TextBox4.Text = "";
+            TextBox5.Text = "";
+            TextBox6.Text = "";
+            TextBox7.Text = "";
+            TextBox8.Text = "";
+
+            ClientScript.RegisterClientScriptBlock(this.GetType(), "K", "alert('Record Updated! Thak you for your valuable time ..')", true);
+        }
+
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+
+            string cs = "Data Source=DESKTOP-NB93CBA\\MSSQLSERVER03;Initial Catalog=Registration;Integrated Security=True";
+            SqlConnection con = new SqlConnection(cs);
+
+            string query = "select * from dbo.data1 where Auto_id='" + (TextBox10.Text) + "'";
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand(query, con);
+
+            SqlDataReader r = cmd.ExecuteReader();
+            while (r.Read())
+            {
+                TextBox10.Text = r.GetValue(1).ToString();
+
+
+
+
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+            ClientScript.RegisterClientScriptBlock(this.GetType(), "K", "alert('Record getted! Thak you for your valuable time ..')", true);
+        }
+
+        private void gvbind()
+        {
+
+
+
+            string sc = "Data Source=DESKTOP-NB93CBA\\MSSQLSERVER03;Initial Catalog=Registration;Integrated Security=True";
+            SqlConnection con = new SqlConnection(sc);
+
+
+            string query = "select * from dbo.data1;";
+
+
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.CommandType = CommandType.Text;
+            con.Open();
+            //cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+            con.Close();
+        }
+
+        protected void GridView1_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+
+
+
+            if (e.CommandName == "Select")
+
+            {
+
+              /*  con.Open();*/
+
+
+                //Determine the RowIndex of the Row whose Button was clicked.
+                int rowIndex = Convert.ToInt32(e.CommandArgument);
+
+                //Reference the GridView Row.
+                GridViewRow row = GridView1.Rows[rowIndex];
+
+                //Fetch value of Name.
+                string Firstname = (row.FindControl("txtName") as TextBox).Text;
+                string Lastname = (row.FindControl("txtlname") as TextBox).Text;
+                string Age = (row.FindControl("Age") as TextBox).Text;
+
+                string Auto_id = (row.FindControl("id") as TextBox).Text;
+
+
+
+
+
+                TextBox5.Text = Firstname;
+
+                TextBox6.Text = Lastname;
+                TextBox7.Text = Age;
+                TextBox8.Text = Auto_id;
+            //    TextBox4.Text = Auto_id;
+
+
+                //Fetch value of Country
+
+
+
+                gvbind();
+
+            }
+
+
+            if (e.CommandName == "Select1")
+
+            {
+
+
+
+                //Determine the RowIndex of the Row whose Button was clicked.
+                int rowIndex = Convert.ToInt32(e.CommandArgument);
+
+                //Reference the GridView Row.
+                GridViewRow row = GridView1.Rows[rowIndex];
+
+                //Fetch value of Name.
+
+
+                string Auto_id = (row.FindControl("id") as TextBox).Text;
+
+
+
+
+
+                // string   welcome = Auto_id;
+
+                //TextBox1.Text = welcome;
+
+
+                string cs = "Data Source=DESKTOP-NB93CBA\\MSSQLSERVER03;Initial Catalog=Registration;Integrated Security=True";
+                SqlConnection con = new SqlConnection(cs);
+
+                string query = "delete from data1 where Auto_id='" + Auto_id + "'";
+
+                SqlCommand cmd = new SqlCommand(query, con);
+
+
+                //cmd.Connection = con;
+
+                con.Open();
+
+                try
+                {
+
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception Ex)
+                {
+                    Console.Write("the user name not found");
+                }
+                con.Close();
+
+                gvbind();
+
+
+
+
+
+
+
+
+
+
+            }
+
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
